49004b1e3316e29bf40a582ba2eac280790a93fe
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-01-27 10:57:19 +0100

    Add project files.

---
+++ Scacchi/Cella.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scacchi
+{
+    class Cella
+    {
+        public int Numero { get; private set;}
+        public char Lettera { get; private set; }
+
+        internal Cella(int n,char l)
+        {
+            Numero = n;
+            Lettera = l;
+        }
+        public Colore Colore
+        {
+            get
+            {
+                bool numeropari = Numero % 2 == 0;
+                bool letterapari = (Lettera-'A') % 2 == 0;
+                if (numeropari == letterapari)
+                    return Colore.Nero;
+                else
+                    return Colore.Bianco;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Lettera}{Numero}";
+        }
+    }
+}
---
+++ Scacchi/Scacchiera.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Scacchi
+{
+    public class Scacchiera
+    {
+        public static Scacchiera Istanza { get; } = new Scacchiera();
+        private Scacchiera()
+        {
+        }
+
+        public List<Cella> GetCelle()
+        {
+            return;
+        }
+        public Cella GetCella(char l,int n)
+        {
+            string l_n = $"{l}{n}";
+
+        }
+    }
+}
